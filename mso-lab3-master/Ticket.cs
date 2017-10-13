903cb4918b31eb52611afea0c3356e693fd6b5c2
Wessel Gordijn <wesselsimon@hotmail.com>
2017-10-13 16:11:39 +0200

    Date added

--- mso-lab3-master/Ticket.cs
+++ mso-lab3-master/Ticket.cs
@@ -2,85 +2,92 @@
 
 namespace Lab3
 {
-	public enum Class
-	{
-		FirstClass,
-		SecondClass
-	}
+    public enum Class
+    {
+        FirstClass,
+        SecondClass
+    }
 
-	public enum Way
-	{
-		OneWay,
-		Return
-	}
+    public enum Way
+    {
+        OneWay,
+        Return
+    }
 
-	public enum Discount
-	{
-		NoDiscount,
-		TwentyDiscount,
-		FortyDiscount
-	}
+    public enum Discount
+    {
+        NoDiscount,
+        TwentyDiscount,
+        FortyDiscount
+    }
 
-	public enum Payment
-	{
-		DebitCard,
-		CreditCard,
-		Cash
-	}
+    public enum Payment
+    {
+        DebitCard,
+        CreditCard,
+        Cash
+    }
 
-	public struct Ticket
-	{
-		string from, to;
-		Class cls;
-		Way way;
-		Discount discount;
-		Payment payment;
+    public struct Ticket
+    {
+        string from, to;
+        Class cls;
+        Way way;
+        Discount discount;
+        Payment payment;
+        DateTime date;
 
-		public Ticket (string from, string to, Class cls, Way way, Discount discount, Payment payment)
-		{
-			this.from = from;
-			this.to = to;
-			this.cls = cls;
-			this.way = way;
-			this.discount = discount;
-			this.payment = payment;
-		}
+        public Ticket(string from, string to, Class cls, Way way, Discount discount, Payment payment)
+        {
+            this.from = from;
+            this.to = to;
+            this.cls = cls;
+            this.way = way;
+            this.discount = discount;
+            this.payment = payment;
+            date = DateTime.Now;
+        }
 
-		public string From {
-			get {
-				return from;
-			}
-		}
+        public string From {
+            get {
+                return from;
+            }
+        }
 
-		public string To {
-			get {
-				return to;
-			}
-		}
+        public string To {
+            get {
+                return to;
+            }
+        }
 
-		public Class TClass {
-			get {
-				return cls;
-			}
-		}
+        public Class TClass {
+            get {
+                return cls;
+            }
+        }
 
-		public Way TWay {
-			get {
-				return way;
-			}
-		}
+        public Way TWay {
+            get {
+                return way;
+            }
+        }
 
-		public Discount TDiscount {
-			get {
-				return discount;
-			}
-		}
+        public Discount TDiscount {
+            get {
+                return discount;
+            }
+        }
 
-		public Payment TPayment {
-			get {
-				return payment;
-			}
-		}
+        public Payment TPayment {
+            get {
+                return payment;
+            }
+        }
+        public DateTime Date { 
+            get {
+                return date;
+                }
+        }
 	}
 }
 
